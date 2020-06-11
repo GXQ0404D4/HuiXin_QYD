449f39fd7ae5cbaca83776605e00d4bc74fa3cfb
宅男大咖 <qiuxinquan1014@163.com>
2020-06-11 14:28:02 +0800

    2020.06.11 实体类数据同步

--- src/main/java/com/ktcn/entity/siemensentity/Peripheral_qt.java
+++ src/main/java/com/ktcn/entity/siemensentity/Peripheral_qt.java
@@ -16,19 +16,19 @@ import lombok.Data;
 @Data
 @Component
 public class Peripheral_qt {
-	private Object WWQT0;   //低压端压力低
-	private Object WWQT1;   //低压端压力高
-	private Object WWQT2;   //低压端手自动
-	private Object WWQT3;   //高压端压力低
-	private Object WWQT4;   //高压端压力高
-	private Object WWQT5;   //高压端手自动
-	private Object WWQT6;   //通讯复位
-	private Object WWQT7;   //变频频率反馈
-	private Object WWQT8;   //低压机1变频
-	private Object WWQT9;   //低压机2变频
+	private Object wwqt0;   //低压端压力低
+	private Object wwqt1;   //低压端压力高
+	private Object wwqt2;   //低压端手自动
+	private Object wwqt3;   //高压端压力低
+	private Object wwqt4;   //高压端压力高
+	private Object wwqt5;   //高压端手自动
+	private Object wwqt6;   //通讯复位
+	private Object wwqt7;   //变频频率反馈
+	private Object wwqt8;   //低压机1变频
+	private Object wwqt9;   //低压机2变频
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private Date QTdata;  
+	private Date qtdata;  
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private Date QTdatatime;
+	private Date qtdatatime;
 
 }
